441b2f49480a7222a6d8be0b74f102c9c14d98fc
yashmhatrezt <60573218+theyashmhatre@users.noreply.github.com>
2024-03-29 19:53:24 +0530

    basic set up for node server, express, typescrpt along with mysql connection

---
+++ db/connection.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "bitespeed-task",
+  multipleStatements: true
+});
+
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Connected to the database!');
+});
+
+module.exports = connection;
---
+++ index.ts
@@ -0,0 +1,17 @@
+// @ts-check
+
+require("dotenv").config();
+import express, { Express, Request, Response } from "express";
+const app: express.Application = express();
+
+const PORT: number = 8000;
+app.use(express.json());
+
+
+app.use("/", require("./routes/identity"))
+
+app.use("*", (req: Request, res: Response) => {
+  res.status(404).json({ msg: "Not Found" });
+});
+
+app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
