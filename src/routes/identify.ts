90f9b111bf7af14a51f70fd492c7dd8fa907365b
yashmhatrezt <60573218+theyashmhatre@users.noreply.github.com>
2024-03-31 04:22:14 +0530

    added linkContact functionality; implemented final scenario from the task

--- src/routes/identify.ts
+++ src/routes/identify.ts
@@ -2,7 +2,7 @@ const connection = require("../db/connection");
 import express, { Express, Request, Response } from "express";
 const router = express.Router();
 import { Contact } from "../interfaces/Contact";
-import { createContact, getContacts } from "../utils/helpers";
+import { linkContacts, createContact, getContacts } from "../utils/helpers";
 
 router.post("/identify", async (req: Request, res: Response) => {
   try {
@@ -23,6 +23,74 @@ router.post("/identify", async (req: Request, res: Response) => {
           "secondaryContactIds": []
         }
       })
+    }
+
+    else {
+
+      // checking if more than 1 primary contact exists
+      const primaryContactsList = contactList.filter((r: any) => r.linkPrecedence === "primary")
+      var isMultiplePrimaryExists: boolean = false;
+
+      // if primary contact > 1; linking rest of the contacts to the oldest primary contact record
+      if (primaryContactsList.length > 1) {
+        isMultiplePrimaryExists = true;
+
+        const primaryId = primaryContactsList[0].id;
+        const updatedContactList = contactList.filter((contact: any) => contact.id !== primaryId).map((contact: any) => contact.id)
+
+        await linkContacts(primaryId, updatedContactList)
+      }
+
+      var obj: any = [];
+
+      //creating contact in db only if both the details are provided
+      if (email && phoneNumber && !isMultiplePrimaryExists) {
+        obj = await createContact({ email, phoneNumber }, contactList, false);
+      }
+
+      var emails: string[] = [];
+      var phoneNumbers: string[] = [];
+      var secondaryContactIds: number[] = [];
+
+      let s = new Set();  // to keep track of duplicate emails & phoneNumbers
+
+      // pushing primary contact's details first
+      if (contactList[0].email.length) emails.push(contactList[0].email);
+      if (contactList[0].phoneNumber.length) phoneNumbers.push(contactList[0].phoneNumber);
+
+      // adding rest of the contact details avoiding duplicates
+      for (var i = 0; i < contactList.length; i++) {
+
+        if (i !== 0) {
+
+          //checking if email & phoneNumber is unique and not null
+          if (!s.has(contactList[i].email) && contactList[i].email) emails.push(contactList[i].email);
+          if (!s.has(contactList[i].phoneNumber) && contactList[i].phoneNumber.length) phoneNumbers.push(contactList[i].phoneNumber);
+
+          secondaryContactIds.push(contactList[i].id);
+        }
+
+        s.add(contactList[i].email)
+        s.add(contactList[i].phoneNumber)
+      }
+
+      // adding the newly added contact's data if multiple primary ids don't exist
+      if (!isMultiplePrimaryExists) {
+        if (!s.has(email) && email.length) emails.push(email)
+        if (!s.has(phoneNumber) && phoneNumber.length) phoneNumbers.push(phoneNumber)
+
+        if (obj.insertId) secondaryContactIds.push(obj.insertId)
+      }
+
+      return res.status(200).json({
+        "contact": {
+          "primaryContactId": contactList[0].id,
+          "emails": emails,
+          "phoneNumbers": phoneNumbers,
+          "secondaryContactIds": secondaryContactIds
+        }
+      })
+
     }
 
   } catch (error) {
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -43,11 +43,24 @@ export const getContacts = async (payload: Contact) => {
   })
 }
 
+
 export const createContact = async (currContact: Contact, contactList: any, isPrimary: boolean) => {
   let query: string = isPrimary ? `insert into contact (email, phoneNumber, linkPrecedence) values ('${currContact.email}', '${currContact.phoneNumber}', 'primary')` :
     `insert into contact (email, phoneNumber, linkPrecedence, linkedId) values ('${currContact.email}', '${currContact.phoneNumber}', 'secondary', '${contactList[0].id}')`
   return new Promise((resolve, reject) => {
     connection.query(query,
+      function (err: unknown, result: unknown) {
+        if (err) reject(err);
+
+        resolve(result);
+      });
+  })
+}
+
+export const linkContacts = async (primaryId: number, contactList: string[]) => {
+  return new Promise((resolve, reject) => {
+    const formattedIds = contactList.map(id => `'${id}'`).join(',')
+    connection.query(`update contact SET linkedId = '${primaryId}', linkPrecedence = 'secondary' WHERE id in (${formattedIds})`,
       function (err: unknown, result: unknown) {
         if (err) reject(err);
 
