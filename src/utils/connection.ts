013e530ae5da7163d1090cdff323064cd5adf64f
yashmhatrezt <60573218+theyashmhatre@users.noreply.github.com>
2024-03-31 05:20:02 +0530

    updated db settings

--- src/utils/connection.ts
+++ src/utils/connection.ts
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "bitespeed-task",
+  host: process.env.HOST_URL,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DB_NAME,
   multipleStatements: true
 });
 
